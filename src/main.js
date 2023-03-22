9e6309e7f9fa1628116d73d9d3f5aa961aca53d2
Calvin.Lin <1345410031@qq.com>
2023-03-22 17:59:09 +0800

    配置router element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from "element-plus";
+import "element-plus/theme-chalk/index.css";
+import App from "./App.vue";
 
-createApp(App).use(store).use(router).mount("#app");
+// createApp.use(ElementUI);
+
+createApp(App).use(ElementUI).use(store).use(router).mount("#app");
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="login">
+  <div id="login">
     <h1>This is an login page</h1>
+    <el-button>默认样式</el-button>
   </div>
 </template>
 <script></script>
-<style></style>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
